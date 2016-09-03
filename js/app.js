d1435435528606501af5002358d286f28c3d92d1
javier Guillen <calamardo.facitec@gmail.com>
2016-09-03 10:27:07 -0400

    tarea5

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//funcion anonina autoejecutable
+(function () {
+ console.log('Hola mundo');
+}());
+//variable de array
+var array = [];
+array.nombre = "String"; //variable de string
+array.numero= 0; //variable de numerico
+array.push('Primer elemento de string');  //elemento de string
+array.push('segundo elemento de string');
+console.log(array);
+var objecto= {};//tipo de objecto
+objecto.descripcion=('hola bebe');// atributo de descripcion
+objecto.cantidad=(14); //atributo de cantidad
+console.log(objecto);
